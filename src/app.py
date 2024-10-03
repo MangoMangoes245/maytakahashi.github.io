209d90cd60e44f8f8eccfdef33eb4ed085446f9c
May <maishatakahashi@gmail.com>
2024-10-03 01:51:55 -0400

    modified a readme and added subpages

--- src/app.py
+++ src/app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/') #if someone visits the homepage 
+@app.route('/') #whenever someone visits the homepage
 def home():
     return render_template('index.html') #returns index.html. 
         #render_template auto-looks in a templates folder
+@app.route('/about') 
+def home():
+    return render_template('about.html')
+@app.route('/events')
+def home():
+    return render_template('events.html') 
 
 if __name__ == "__main__":
     app.run(debug=True) #starts the server
