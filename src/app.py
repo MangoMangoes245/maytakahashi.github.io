5c808450b39a58d490119feff9bed24fd80e75b6
May <maishatakahashi@gmail.com>
2024-10-03 02:59:45 -0400

    ahhhh

--- src/app.py
+++ src/app.py
@@ -6,12 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html') #returns index.html. 
         #render_template auto-looks in a templates folder
-@app.route('/about') 
-def about():
-    return render_template('about.html')
-@app.route('/events')
-def events():
-    return render_template('events.html') 
 
 if __name__ == "__main__":
     app.run(debug=True) #starts the server
