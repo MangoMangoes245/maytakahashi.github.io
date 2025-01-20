f9398ac468ce13b751d70d8679241d62ef0229b7
Talha Choudry <talhachoudry16@gmail.com>
2025-01-20 00:43:01 -0800

    finished contact form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,21 @@ import {useState, useEffect} from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
-import Contact from "./pages/Contact";
+import Contact from "./pages/contactform";
 import Mission from "./pages/Mission";
 import "./assets/css/App.css"
 import axios from "axios";
 
 const App = () => {
 
-  const fetchAIP = async () => {
-    const response = await axios.get("http://localhost:8080/api");
-    console.log(response.data.message);
-  };
+  // const fetchAIP = async () => {
+  //   const response = await axios.get("http://localhost:5173/api");
+  //   console.log(response.data.message);
+  // };
 
-  useEffect(() => {
-    fetchAIP();
-  }, []);
+  // useEffect(() => {
+  //   fetchAIP();
+  // }, []);
 
   return (
     <Router>
--- server/server.js
+++ server/server.js
@@ -1,16 +1,89 @@
 const express = require('express');
-const app = express();
+const nodeMailer = require('nodemailer');
 const cors = require('cors');
-const corsOptions = {
-    origin: 'http://localhost:5173'
-};
+const app = express();
+const port = 3000;
 
-app.use(cors(corsOptions));
+const myemail = 'ruorigamiclub@gmail.com'
+const mypassword = 'hzjjtfzcosctjiqy'
 
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello, world!' });
+app.use(cors());
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({extended: true, limit: '25mb'}));
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
 });
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+function sendEmail({ receiver_email, subject, message }) {
+    return new Promise((resolve, reject) => {
+        var transporter = nodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: myemail,
+                pass: mypassword
+            },
+        });
+    
+        const mailOptions = {
+            from: myemail,
+            to: myemail,
+            subject: subject,
+            text: message,
+            replyTo: receiver_email,
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+                return reject({message: 'an error occurred'});
+            } else {
+                return resolve ({message: 'email sent'});
+            }
+        });
+    });
+}
+
+app.get("/", (req, res) => {
+    sendEmail()
+        .then((response) => res.send(response.message))
+        .catch((error) => res.status(500).send(error.message));
 });
+
+
+app.post("/send_email", (req, res) => {
+    const { receiver_email, subject, message } = req.body; 
+    if (!receiver_email || !subject || !message) {
+        return res.status(400).send("All fields are required");
+    }
+    sendEmail(req.body)
+        .then((response) => res.send(response.message))
+        .catch((error) => res.status(500).send(error.message));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+// const cors = require('cors');
+// const corsOptions = {
+//     origin: 'http://localhost:5173'
+// };
+
+// app.use(cors(corsOptions));
+
+// const PORT = process.env.PORT || 8080;
+
+// //Middleware
+// app.use(express.static('public'));
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/contactform.jsx');
+// });
+// // app.get('/api', (req, res) => {
+// //     res.json({ message: 'Hello, world!' });
+// // });
+
+// app.listen(8080, () => {
+//     console.log('Server is running on port 8080');
+// });
