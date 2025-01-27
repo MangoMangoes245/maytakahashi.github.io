f4892a363cc70f842065c21484fd7f4144019ddd
Talha Choudry <talhachoudry16@gmail.com>
2025-01-26 16:55:49 -0800

    test server vercel

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const nodeMailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 9000;
 
 const myemail = 'ruorigamiclub@gmail.com'
 const mypassword = 'hzjjtfzcosctjiqy'
