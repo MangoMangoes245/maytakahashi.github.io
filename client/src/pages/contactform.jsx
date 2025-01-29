627952b60c848285f0c625be621cbd8e9a1431f3
Talha Choudry <talhachoudry16@gmail.com>
2025-01-28 17:13:58 -0800

    test server vercel

--- client/src/pages/contactform.jsx
+++ client/src/pages/contactform.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
       return alert("Please fill in all fields");
     } else {
       try {
-        let result = await axios.post("https://maytakahashi-github-io-server-f2tl0f6s4.vercel.app", { // Use the correct deployed server URL
+        let result = await axios.post("maytakahashi-github-io-proxy-9n07a8dmc.vercel.app", { // Use the correct deployed server URL
           receiver_email: receiver_email,
           subject: subject,
           message: message,
