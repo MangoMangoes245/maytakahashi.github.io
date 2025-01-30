fcbdb29df491ab1d89e0aad4bd283018efc2588a
Talha Choudry <talhachoudry16@gmail.com>
2025-01-29 18:46:11 -0800

    test server vercel

---
+++ proxyServer/index.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const API_URL = process.env.API_URL; // Ensure this is the correct backend server URL
+const CLIENT_URL = process.env.CLIENT_URL; // Ensure this is the correct client URL
+
+module.exports = async (req, res) => {
+  try {
+    console.log('Incoming request:', req.method, req.url);
+
+    // Extract query parameters from the incoming request
+    const queryString = req.url.split('?')[1] || '';
+    console.log('Query string:', queryString);
+
+    const response = await axios.get(`${API_URL}${req.path}?${queryString}`);
+    console.log('Response from API:', response.data);
+
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Send the response data
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error occurred:', error.message);
+    res.status(500).json({ type: 'error', message: error.message });
+  }
+};
