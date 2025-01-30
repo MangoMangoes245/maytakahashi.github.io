a2d2b56dc73ac1de4772d5ea44411a436f950e62
Talha Choudry <talhachoudry16@gmail.com>
2025-01-29 18:36:17 -0800

    test server vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,16 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/proxy': {
-        target: 'https://maytakahashi-github-io-server-f2tl0f6s4.vercel.app', // Replace with your backend server URL
+      '/*': {
+        target: 'https://maytakahashi-github-io-server-qr1v19b4y.vercel.app', // Replace with your backend server URL for local development
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/proxy/, '')
-      },
-      '/send_email': {
-        target: 'https://maytakahashi-github-io-server-2lhef7esx.vercel.app', // Replace with your backend server URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/send_email/, '/send_email')
-      }
+        secure: false,
+        }
     }
   }
 });
