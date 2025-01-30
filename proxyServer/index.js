6614d743554e86b0ba122e1d0802e916f1bd345a
Talha Choudry <talhachoudry16@gmail.com>
2025-01-29 18:49:20 -0800

    test server vercel

--- proxyServer/index.js
+++ proxyServer/index.js
@@ -9,7 +9,7 @@ const CLIENT_URL = process.env.CLIENT_URL; // Ensure this is the correct client 
 module.exports = async (req, res) => {
   try {
     console.log('Incoming request:', req.method, req.url);
-
+    
     // Extract query parameters from the incoming request
     const queryString = req.url.split('?')[1] || '';
     console.log('Query string:', queryString);
@@ -18,7 +18,7 @@ module.exports = async (req, res) => {
     console.log('Response from API:', response.data);
 
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.setHeader('Access-Control-Allow-Origin', CLIENT_URL); // Replace with your client domain
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
